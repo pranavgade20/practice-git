22ed8cdd24d09cb65c9ac54bd46a23fb64e79cde
anshu <lcb2021043@iiitl.ac.in>
2022-03-11 11:45:47 +0530

    hello
    hello jjk

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+<<<<<<< HEAD
+    cout<<"Hello, World";
+=======
+    <<"hello, git";
+>>>>>>> hello-git
+    return 0;
+}
---
+++ hellos/hellogit.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello git";
+    return 0;
+}
