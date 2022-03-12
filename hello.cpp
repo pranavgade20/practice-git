a4d616c90de4d3d3f8c3e8b6b57c895bf589a598
Hardik Khandelwal <hardikkhandelwal@Hardik-Khandelwal.local>
2022-03-12 14:57:18 +0530

    squashing and merging

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,14 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
 cout<<"Hello, World";
 return 0;
 }
+
+#include<bits/stdc++.h> 
+using namespace std; 
+int main(){ 
+cout<<"Hello, Git";
+return 0;
+} 
